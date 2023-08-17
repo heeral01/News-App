a125190efb87e1c0489eebebc1c80133ddd85400
heeral01 <117432070+heeral01@users.noreply.github.com>
2023-08-17 11:58:53 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,27 @@
+
+import { BrowserRouter,Routes,Route } from 'react-router-dom';
+import './App.css';
+
+import Navbar from './components/navbar';
+import Home from './components/home';
+import About from './components/about';
+import Contact from './components/contact';
+import Form from './components/form';
+function App() {
+  return (
+    <div className="App">
+     <BrowserRouter>
+      <Navbar />
+      <Routes>
+       <Route path="/" element={<Home />}/>
+       <Route path="/about" element={<About />}/>
+       <Route path="/contact" element={<Contact />}/>
+       <Route path="/form" element={<Form />}/>
+      </Routes>
+     </BrowserRouter> 
+     
+    </div>
+  );
+}
+
+export default App;
